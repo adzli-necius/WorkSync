706d665b2c4e2aca1efb9125cfa68709f28026c0
adzli-necius <muhdadzlijefry@gmail.com>
2026-06-30 14:41:19 +0800

    feat(database): create department table and seed initial reference data

---
+++ src/main/resources/db/migration/V3__seed_role_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO role (code, name)
+VALUES
+    ('ADMIN','Administrator'),
+    ('HR','Human Resource'),
+    ('MANAGER','Manager'),
+    ('EMPLOYEE','Employee');
---
+++ src/main/resources/db/migration/V4__seed_department_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (
+    code,
+    name,
+    description
+)
+VALUES
+    ('OPS', 'Operations', 'Warehouse Operations'),
+    ('WH', 'Warehouse', 'Warehouse Management'),
+    ('LOG', 'Logistics', 'Transportation and Logistics'),
+    ('HR', 'Human Resource', 'Human Resource Department'),
+    ('FIN', 'Finance', 'Finance and Accounting'),
+    ('IT', 'Information Technology', 'IT Support and Infrastructure');
